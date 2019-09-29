7d7f19528f17037231e34d939c246b511ded6c97
WaterKat <waterkatgames@gmail.com>
2019-09-29 01:31:05 -0700

    Added Particles and Shaders

---
+++ Assets/RotateWithMouse.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateWithMouse : MonoBehaviour
+{
+    private void Update()
+    {
+        transform.rotation *= Quaternion.Euler(Input.GetAxis("Mouse Y")*2, Input.GetAxis("Mouse X")*2, 0);
+        transform.position += transform.forward * 2;
+    }
+}
---
+++ Assets/WaterKat/ColorSchemes/ColorPallete.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System;
+using UnityEngine;
+
+
+namespace WaterKat.WKColor
+{
+    [CreateAssetMenu(fileName = "New_ColorPallete", menuName = "WaterKat/WKColor/ColorPallete", order = 1)]
+    public class ColorPallete : ScriptableObject
+    {
+        [Serializable]
+        public struct ColorScheme
+        {
+            public string colorName;
+            public Color color;
+        }
+        [SerializeField]
+        List<ColorScheme> colorSchemes;
+    }
+}

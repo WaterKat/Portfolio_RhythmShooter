e072df7d1eea513c3e31db0690d14854a72b08a4
WaterKat <waterkatgames@gmail.com>
2020-01-14 09:55:12 -0800

    MinorUpdate

---
+++ Assets/AdvancedAudio.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AdvancedAudio : MonoBehaviour
+{
+    int AudioSampleRate = 44100;
+    
+    public int PrecisePosition(AudioSource audioSource)
+    {
+        return audioSource.timeSamples;
+    }
+    public double deltaDoublePosition(AudioSource audioSource)
+    {
+        return (double)audioSource.timeSamples / (double)audioSource.clip.samples;
+    }
+
+    public class BeatMap
+    {
+        public int[] beatPositions;
+
+        void GenerateBasicBeatMap(int bpm, int sampleLength, int sampleOffset)
+        {
+            int offsetLength = (sampleLength - sampleOffset);
+            double bps = bpm / 60;
+
+        }
+    }
+}
